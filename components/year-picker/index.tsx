f373446c05802b66dcafb17a2827d0230b716116
yu_hu <huyu@esrichina.com.cn>
2021-06-04 18:01:05 +0800

    年份选择器

--- components/year-picker/index.tsx
+++ components/year-picker/index.tsx
@@ -4,23 +4,43 @@
  * @DATE: 2021-06-01
  */
 import * as React from "react";
-import {Moment} from "moment";
+import moment, {Moment} from "moment";
 import Picker from "rc-picker";
 import {PickerBaseProps} from "rc-picker/lib/Picker";
 import momentGenerateConfig from 'rc-picker/lib/generate/moment';
 import SizeContext from 'antd/lib/config-provider/SizeContext';
 import LocaleReceiver from "antd/lib/locale-provider/LocaleReceiver";
-import DoubleLeftOutlined from '@ant-design/icons/DoubleLeftOutlined';
-import DoubleRightOutlined from '@ant-design/icons/DoubleRightOutlined';
+import {CloseCircleFilled, DoubleLeftOutlined, DoubleRightOutlined} from '@ant-design/icons';
 import zhCN from 'rc-picker/lib/locale/zh_CN';
 
 export type PickerProps = PickerBaseProps<string>
 
 export interface YearPickerProp {
-
+  value?: string;
+  defaultValue?: string;
+  onChange?: (value?: string) => void;
+  classNames?: string;
+  style?: React.CSSProperties;
+  size?: 'large' | 'middle' | 'small';
 }
 
-const YearPicker: React.FC<YearPickerProp> = (props) => {
+const YearPicker: React.FC<YearPickerProp> = (
+  {
+    classNames,
+    value,
+    defaultValue,
+    onChange,
+    ...restProps
+  }
+) => {
+  const overrideProps: any = {};
+
+  if (value) {
+    overrideProps.value = moment(value);
+  }
+  if (defaultValue) {
+    overrideProps.defaultValue = moment(defaultValue);
+  }
 
   const renderPicker = () => {
     return (
@@ -29,19 +49,19 @@ const YearPicker: React.FC<YearPickerProp> = (props) => {
           return (
             <Picker<Moment>
               picker='year'
-              // clearIcon={<CloseCircleFilled/>}
               allowClear
-              // transitionName="slide-up"
+              transitionName="slide-up"
+              clearIcon={<CloseCircleFilled/>}
               prevIcon={<DoubleLeftOutlined/>}
               nextIcon={<DoubleRightOutlined/>}
               generateConfig={momentGenerateConfig}
               locale={zhCN}
-              // className=
-              // value={moment('2010')}
-              // onSelect={}
               prefixCls='antd-ext-year-picker'
-              // superPrevIcon={<span className={`${prefixCls}-super-prev-icon`} />}
-              // superNextIcon={<span className={`${prefixCls}-super-next-icon`} />}
+              superPrevIcon={<DoubleLeftOutlined/>}
+              superNextIcon={<DoubleRightOutlined/>}
+              onChange={(date, dateString) => onChange && onChange(dateString)}
+              {...restProps}
+              {...overrideProps}
             />
           )
         }}
--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ export default {
   files: ['./components/**/*.mdx', "./components/*.mdx"],
   // files: ['./src/components/**/*.mdx','./src/*.mdx'],
   // dest: './site', // 打包 docz 文档到哪个文件夹下
-  title: 'Huyoo UI',  // 设置文档的标题
+  title: 'Antd Extend UI',  // 设置文档的标题
   typescript: true, // 支持 typescript 语法
   themesDir: 'theme', // 主题样式放在哪个文件夹下，后面会讲
   menu: ['快速上手','组件',] // 生成文档的左侧菜单分类
